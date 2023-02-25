75c944ed1378da2c7f489468e1aed9dd60c2bf12
Ninju <alex.watt@rvu.co.uk>
2023-02-25 10:52:48 +0000

    Added home.nix (coupled to this machine for now)

--- home.nix
+++ home.nix
@@ -1,53 +1,23 @@
 {pkgs, ...}: {
-    home.username = "alex";
-    home.homeDirectory = "/home/{home.username}";
-    home.stateVersion = "22.11"; # To figure this out you can comment out the line and see what version it expected.
-    programs.home-manager.enable = true;
+  home.username = "alex";
+  home.homeDirectory = "/home/{home.username}";
+  home.stateVersion = "22.11"; # To figure this out you can comment out the line and see what version it expected.
+  programs.home-manager.enable = true;
 
-    home.sessionVariables = {
-	    NIX_CONFIG_PATH = "~/.config/nixconfig";
-    };
-
-    home.packages = [
-	pkgs.bat
-	pkgs.neovim
-	pkgs.tree
-    ];
-
-
-
-programs.git = {
-    enable = true;
-    userName = "Ninju";
-    userEmail = "alex.watt@rvu.co.uk";
-    aliases = {
-      unstage = "reset HEAD --";
-      pr = "pull --rebase";
-      co = "checkout";
-      ci = "commit";
-      br = "branch";
-      st = "status";
-      gong = "commit --amend --no-edit";
-      gianf = "push --force-with-lease";
-      l = "log --graph --pretty='%Cred%h%Creset - %C(bold blue)<%an>%Creset %s%C(yellow)%d%Creset %Cgreen(%cr)' --abbrev-commit --date=relative";
-      lg = "log --graph --pretty='%Cred%h%Creset - %C(bold blue)<%an>%Creset %s%C(yellow)%d%Creset %Cgreen(%cr)' --abbrev-commit --date=relative";
-      pp = "!git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)";
-      recent-branches = "branch --sort=-committerdate";
-      current-branch = "rev-parse --abbrev-ref HEAD";
-    };
-    extraConfig = {
-      core = { editor = "nvim"; };
-      rerere.enabled = true;
-
-      # Fixes getting dependencies from private repos in Golang
-      url."git@github.com:".insteadOf = "https://github.com/";
-    };
-#   signing = {
-#     signByDefault = true;
-#     key = "************";
-#   };
-
-    ignores = ["*.swp" "*~"];
+  home.sessionVariables = {
+    NIX_CONFIG_PATH = "~/.config/nixconfig";
   };
-	
+
+  home.packages = [
+    pkgs.bat
+    pkgs.tree
+    pkgs.wget
+    pkgs.xclip
+  ];
+
+  imports = [
+    ./programs/neovim.nix
+    ./programs/git.nix
+    ./programs/kitty.nix
+  ];
 }
