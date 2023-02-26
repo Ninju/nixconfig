829249882d3ca64ce59814591e1ac0a6f6f82928
Ninju <alex.watt@rvu.co.uk>
2023-02-26 21:15:58 +0000

    Add extend-like layer to Keyboard for CAPS

--- nixos-configuration/config_files/kmonad.lisp
+++ nixos-configuration/config_files/kmonad.lisp
@@ -1,28 +1,59 @@
-(defsrc
-  grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
-  tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
-  caps a    s    d    f    g    h    j    k    l    ;    '    ret
+;; Best to make sure that no setxkbmap variant is set (i.e. it's qwerty)
+;; Otherwise weird stuff happens no matter what `defsrc` is set to..
+
+(defsrc ;; qwerty
+  esc  f1  f2  f3  f4  f5  f6  f7  f8  f9  f10 f11 f12 home end ins del
+  `    1    2    3    4    5    6    7    8    9    0    -    =     bspc
+  tab  q    w    e    r    t    y    u    i    o    p    [    ]
+  caps a    s    d    f    g    h    j    k    l    ;    '          ret
   lsft z    x    c    v    b    n    m    ,    .    /    rsft
-  lctl lmet lalt           spc            ralt rmet cmp  rctl
+       lctl lmet lalt       spc           ralt prnt rctl  pgup up   pgdn
+                                                          lft  down rght
 )
 
 (defalias
-  num  (layer-toggle numbers) ;; Bind num to a button that switches to a layer
-  kil  C-A-del                ;; Bind kil to a button that Ctrl-Alt-deletes
+  ext  (tap-next esc (layer-toggle extend))
+
+  cpy C-S-c
+  pst C-S-v
 )
 
-(deflayer qwerty
-  grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
-  tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
-  caps a    s    d    f    g    h    j    k    l    ;    '    ret
-  lsft z    x    c    v    b    n    m    ,    .    /    rsft
-  lctl @num lalt           spc            ralt rmet cmp rctl
+(deflayer colemak
+  esc  f1  f2  f3  f4  f5  f6  f7  f8  f9  f10 f11 f12 home end ins del
+  `    1    2    3    4    5    6    7    8    9    0    -    =     bspc
+  tab  q    w    f    p    g    j    l    u    y    ;    [    ]
+  @ext a    r    s    t    d    h    n    e    i    o    '          ret
+  lsft z    x    c    v    b    k    m    ,    .    /    rsft
+       lctl lmet lalt       spc           ralt prnt rctl  pgup up   pgdn
+                                                          lft  down rght
 )
 
-(deflayer numbers
-  _    _    _    _    _    _    _    _    _    _    _    _    _    _
-  _    _    _    _    _    XX   /    7    8    9    -    _    _    _
-  _    _    _    _    _    XX   *    4    5    6    +    _    _
-  _    _    \(   \)   .    XX   0    1    2    3    _    _
-  _    _    _              _              _    _    _    _
+(deflayer colemak-2
+  esc  f1  f2  f3  f4  f5  f6  f7  f8  f9  f10 f11 f12 home end ins del
+  `    1    2    3    4    5    6    7    8    9    0    -    =     bspc
+  tab  q    w    f    p    g    j    l    u    y    ;    [    ]
+  @ext a    r    s    t    d    h    n    e    i    o    '          ret
+  lsft z    x    c    v    b    k    m    ,    .    /    rsft
+       lctl lmet lalt       spc           ralt prnt rctl  pgup up   pgdn
+                                                          lft  down rght
+)
+
+(deflayer extend
+  _    _   _   _   _   _   _   _   _   _   _   _   _   _    _   _   _
+  _    _    _    _    _    _    _    _    _    _    _    _    _     _
+  _    _    _    _    _    [    ]    home up   end  _    _    _
+  _    lalt _    lsft lctl \(   \)   lft  down rght bspc _          _
+  _    _    _    @cpy @pst {    }    _    _    _    _    _
+       _    _    _          ret           _    _    _     _    _    _
+                                                          _    _    _
+)
+
+(deflayer empty
+  _    _   _   _   _   _   _   _   _   _   _   _   _   _    _   _   _
+  _    _    _    _    _    _    _    _    _    _    _    _    _     _
+  _    _    _    _    _    _    _    _    _    _    _    _    _
+  _    _    _    _    _    _    _    _    _    _    _    _          _
+  _    _    _    _    _    _    _    _    _    _    _    _
+       _    _    _          _             _    _    _     _    _    _
+                                                          _    _    _
 )
