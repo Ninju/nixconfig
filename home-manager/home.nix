16b1d87a4ae129a4edfdabe9d3e5265163d50fbc
Ninju <alex.watt@rvu.co.uk>
2023-02-26 21:17:47 +0000

    Add more useful packages in home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,4 +1,5 @@
-{pkgs, ...}: {
+{pkgs, config, ... }:
+{
   home.username = "alex";
   home.homeDirectory = "/home/{home.username}";
   home.stateVersion = "22.11"; # To figure this out you can comment out the line and see what version it expected.
@@ -8,11 +9,20 @@
     NIX_CONFIG_PATH = "~/.config/nixconfig";
   };
 
-  home.packages = [
-    pkgs.bat
-    pkgs.tree
-    pkgs.wget
-  ];
+  # https://nix.dev/anti-patterns/language#with-attrset-expression
+  home.packages = builtins.attrValues {
+    inherit (pkgs)
+    bat
+    silver-searcher
+    tree
+    wget
+    htop
+    jq
+    yq
+    csvkit
+    feh
+    fzf
+    ; };
 
   imports = [
     ./programs/neovim.nix
