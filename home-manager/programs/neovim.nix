dc1877e90c8ff3c71442a29a65debf086ab42c39
Ninju <alex.watt@rvu.co.uk>
2023-02-26 11:54:11 +0000

    Remove duplicate packages in neovim spec

--- home-manager/programs/neovim.nix
+++ home-manager/programs/neovim.nix
@@ -11,11 +11,8 @@
     extraConfig = builtins.readFile ./config_files/vimrc; 
 
     plugins = with pkgs.vimPlugins; [
-      bufexplorer
       ctrlp
-      vim-fireplace
       bufexplorer
-      vim-surround
       nerdtree
       vim-gitgutter
       tagbar
@@ -23,10 +20,7 @@
       vim-surround
       vim-fireplace
       vim-salve
-      nerdtree
-      vim-gitgutter
       vim-visual-increment
-      tagbar
       vim-go
       vim-nix
     ];
