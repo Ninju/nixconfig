038c62353916f0ce04b3ba7f8b42956911c1c8b4
Ninju <alex.watt@rvu.co.uk>
2023-03-03 20:12:34 +0000

    Clean up keymap config in configuration.nix

--- nixos-configuration/configuration.nix
+++ nixos-configuration/configuration.nix
@@ -53,11 +53,8 @@
   # services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "gb";
-    # xkbVariant = "colemak";
-  };
+  # Full keymap is handled by KMonad
+  services.xserver.layout = "gb";
 
   # Configure console keymap
   console.keyMap = "uk";
