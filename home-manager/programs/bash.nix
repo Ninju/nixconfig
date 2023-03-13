3047877e77fff8e6419ccccdd6cbf4e90a5da790
Ninju <alex.watt@rvu.co.uk>
2023-03-13 19:37:49 +0000

    Set $TERMINAL to make i3 errors show in kitty
    (instead of urxvt)

--- home-manager/programs/bash.nix
+++ home-manager/programs/bash.nix
@@ -6,6 +6,8 @@
       DOTFILES_DIR = "/home/alex/src/git/Ninju/nixconfig";
       DOTFILES_REMOTE_SSH = "git@github.com:Ninju/nixconfig";
       DOTFILES_REMOTE_HTTPS = "https://www.github.com/Ninju/nixconfig";
+
+      TERMINAL = "${pkgs.kitty}/bin/kitty";
     };
 
     shellAliases = {
