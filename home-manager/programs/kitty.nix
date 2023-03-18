92a17e64f6ac06ae3ea99711e340b42fe66b300e
Ninju <alex.watt@rvu.co.uk>
2023-03-18 11:26:33 +0000

    Use built in themes for kitty

--- home-manager/programs/kitty.nix
+++ home-manager/programs/kitty.nix
@@ -1,14 +1,8 @@
 { pkgs, specialArgs, ... }:
-let
-  themes = "${specialArgs.kitty-themes}/themes";
-in
 {
   programs.kitty = {
     enable = true;
     theme = "Aquarium Dark";
-    extraConfig = builtins.readFile ./config_files/kitty + ''
-
-    include ${themes}/Github.conf
-    '';
+    extraConfig = builtins.readFile ./config_files/kitty;
   };
 }
