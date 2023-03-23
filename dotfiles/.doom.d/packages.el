b817c1a944af7e1f5eb2b00c21a741514b5fdc7e
Ninju <alex.watt@rvu.co.uk>
2023-03-23 21:25:11 +0000

    Add kbd-mode for KMonad config files

--- dotfiles/.doom.d/packages.el
+++ dotfiles/.doom.d/packages.el
@@ -69,6 +69,9 @@
 (package! cue-mode)
 (package! restclient)
 (package! ox-twbs)
+(package! kbd-mode
+  :recipe (:host github
+           :repo "kmonad/kbd-mode"))
 
 (after! simple-httpd
   (setq httpd-port 7070)
