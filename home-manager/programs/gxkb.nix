f8ab515a51110200cf2af7a8c6c86ebc9a5ee259
Ninju <alex.watt@rvu.co.uk>
2023-03-23 20:09:57 +0000

    Store GXKB config in separate file

--- home-manager/programs/gxkb.nix
+++ home-manager/programs/gxkb.nix
@@ -1,16 +1,6 @@
 { config, lib, pkgs, ... }:
 {
-  home.file.".config/gxkb/gxkb.cfg".source = pkgs.writeText "gxkb.cfg" ''
-    [xkb config]
-    group_policy=2
-    default_group=0
-    never_modify_config=false
-    model=pc104
-    layouts=gb,de,ua,ru
-    variants=,,,
-    toggle_option=,,,
-    compose_key_position=;
-  '';
+  home.file.".config/gxkb/gxkb.cfg".source = ../../dotfiles/.config/gxkb/gxkb.cfg;
 
   home.packages = [
     pkgs.gxkb
