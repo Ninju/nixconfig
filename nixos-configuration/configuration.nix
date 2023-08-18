5bb75833a0bfb129ade68b1ebdac33fa8bf4d608
Ninju <alex.watt@rvu.co.uk>
2023-08-18 19:14:24 +0100

    Add support for HP Printer (Home)

--- nixos-configuration/configuration.nix
+++ nixos-configuration/configuration.nix
@@ -113,6 +113,12 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.drivers = [ pkgs.hplip ];
+
+  services.avahi.enable = true;
+  # Important to resolve .local domains of printers, otherwise you get an error
+  # like  "Impossible to connect to XXX.local: Name or service not known"
+  services.avahi.nssmdns = true;
 
   # Enable sound with pipewire.
   sound.enable = true;
